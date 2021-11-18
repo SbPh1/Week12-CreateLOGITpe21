2cbf7c0400fcda008eb0219c911cc9a2b0bb41bb
H <sikkelh@gmail.com>
2021-11-18 10:38:15 +0200

    Tasks

---
+++ CreateLOGITpe21/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace CreateLOGITpe21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string newDiretory = "LOGITpe21";
+
+            CreateMyDirectory(newDiretory);
+           
+        }
+        private static void CreateMyDirectory(string myDirectoryName)
+        {
+            string rootDirectory = @"C:\Users\Monkey\samples";
+            string directoryFullPath = $@"{rootDirectory}\{myDirectoryName}";
+
+            if (Directory.Exists(directoryFullPath))
+            {
+                Console.WriteLine($"Directory {myDirectoryName} already exists in {directoryFullPath}.");
+            }
+
+            else
+            {
+                Directory.CreateDirectory(directoryFullPath);
+                Console.WriteLine($"Directory {myDirectoryName} has been created.");
+            }
+        }
+
+
+    }
+}
